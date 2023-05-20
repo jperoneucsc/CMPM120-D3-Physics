59dc5b0141a26dde55e2ce8d8ac9ca8bcc327ca4
jperone <jperone@ucsc.edu>
2023-05-19 21:23:41 -0700

    Fix net

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ let config ={
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
+    backgroundColor: '0x101010',
     physics: {
         default: 'matter',
         matter: {
@@ -9,7 +10,9 @@ let config ={
                 y: 0.8
             },
             debug: true,
-            debugBodyColor: 0xffffff
+            debugShowBody: true,
+            debugBodyColor: 0x000000,
+            debugStaticBodyColor: 0x000000
         }
     },
     scene: [Scene1]
